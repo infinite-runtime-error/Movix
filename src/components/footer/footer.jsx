8cb9498d12e168ef186d67114e55a776d3e2394b
infinite-runtime-error <infiniteruntimeerror@gmail.com>
2024-02-08 23:36:08 +0530

    Movix Code Completed

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                🚀 Welcome to Movix! This frontend project has been meticulously crafted with a passion for learning. Every line of code represents a step in the exciting journey of mastering web development. Dive into the codebase, explore, and enjoy the process of discovery. Built for the sole purpose of learning, this project is a testament to continuous growth and improvement. Happy coding!
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/utils/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+const headers = 
+{
+    Authorization : "bearer " + TMDB_TOKEN,
+}
+
+export const fetchDataFromApi = async(url,params)=>
+{
+    try
+    {
+        //console.log("TMDB TOKEN IS ",TMDB_TOKEN);
+        const {data} = await axios.get(BASE_URL + url,{
+            headers,
+            params
+        })
+
+        return data;
+    }
+    catch(err)
+    {
+        console.log(err);
+        return err;
+    }
+}
